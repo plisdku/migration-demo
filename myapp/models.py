270d66165fd1f914e7941b637aafbcab6ed7851f
Paul Hansen <paul.c.hansen@gmail.com>
2024-01-07 15:58:09 -0800

    coupla migrations

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=20)
+    person_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=40)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
